b12ac03264cf97220cab5be59f39dfc0bc8e7b85
Joao-Arthur-carvassun <joaaomgalaxs@gmail.com>
2025-08-05 17:51:53 +0000

    Desafio: nível novato,Criando as Cartas do Super Trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,16 +7,70 @@
 //Teste larissa
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+char letra_cidade1;
+ char letra_numero1[10];
+ char nome_cidade1[20];
+ int numero_de_habitantes1;
+ float Area_cidade1;
+ float Pib1;
+ int Pontos_turisticos1;
 
-    return 0;
+ char letra_cidade2;
+ char letra_numero2[10];
+ char nome_cidade2[20];
+ int numero_de_habitantes2;
+ float Area_cidade2;
+ float Pib2;
+ int Pontos_turisticos2;
+
+
+printf("Digite uma letra de A a H para o estado: \n");
+scanf("%c",&letra_cidade1);
+
+printf("Digite a letra ja digitada mais um numero entre 01 e 04 juntos: \n");
+scanf("%s",&letra_numero1);
+
+printf("Digite o nome da cidade: \n");
+scanf("%s",&nome_cidade1);
+
+printf("NUmero de habitantes: \n");
+scanf("%i",&numero_de_habitantes1);
+
+printf("Digite a area da cidade: \n");
+scanf("%f",&Area_cidade1);
+
+printf("Digite seu respectivo PIB: \n");
+scanf("%f",&Pib1);
+
+printf("Digite quantos pontos turisticos: \n");
+scanf("%i",&Pontos_turisticos1);
+
+printf("Agora os dados da cidade 2\n");
+
+printf("Digite uma letra de A a H para o estado: \n");
+scanf(" %c",&letra_cidade2);
+
+printf("Digite a letra ja digitada mais um numero entre 01 e 04 juntos: \n");
+scanf("%s",&letra_numero2);
+
+printf("Digite o nome da cidade: \n");
+scanf("%s",&nome_cidade2);
+
+printf("NUmero de habitantes: \n");
+scanf("%i",&numero_de_habitantes2);
+
+printf("Digite a area da cidade: \n");
+scanf("%f",&Area_cidade2);
+
+printf("Digite seu respectivo PIB: \n");
+scanf("%f",&Pib2);
+
+printf("Digite quantos pontos turisticos: \n");
+scanf("%i",&Pontos_turisticos2);
+
+printf("Carta 1:\nEstado:%c\nCódigo:%s\nNome da Cidade:%s\nPopulação:%i\nÁrea:%f\nPIB:%f\nNúmero de Pontos Turísticos:%i\n",letra_cidade1,letra_numero1,nome_cidade1,numero_de_habitantes1,Area_cidade1,Pib1,Pontos_turisticos1);
+
+printf("Carta 2:\nEstado:%c\nCódigo:%s\nNome da Cidade:%s\nPopulação:%i\nÁrea:%f\nPIB:%f\nNúmero de Pontos Turísticos:%i\n",letra_cidade2,letra_numero2,nome_cidade2,numero_de_habitantes2,Area_cidade2,Pib2,Pontos_turisticos2);
+    
+return 0;
 }
