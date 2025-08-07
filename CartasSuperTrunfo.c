590452e4ea2a330c3031d0386e1c49dac1697ddc
Joao-Arthur-carvassun <joaaomgalaxs@gmail.com>
2025-08-07 00:30:01 +0000

    Desafio: nível mestre,Batalha de Cartas no Super Trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,17 +9,17 @@
 int main(){
    
  char letra_cidade1;
- char letra_numero1[10];
- char nome_cidade1[20];
- int numero_de_habitantes1;
+ char Codigo1[10];
+ char Cidade[20];
+ unsigned long int populacao1;
  float Area_cidade1;
  float Pib1;
  int Pontos_turisticos1;
 
  char letra_cidade2;
- char letra_numero2[10];
- char nome_cidade2[20];
- int numero_de_habitantes2;
+ char Codigo2[10];
+ char Cidade2[20];
+ unsigned long int poúlacao2;
  float Area_cidade2;
  float Pib2;
  int Pontos_turisticos2;
@@ -29,13 +29,13 @@ printf("Digite uma letra de A a H para o estado: \n");
 scanf("%c",&letra_cidade1);
 
 printf("Digite a letra ja digitada mais um numero entre 01 e 04 juntos: \n");
-scanf("%s",&letra_numero1);
+scanf("%s",Codigo1);
 
 printf("Digite o nome da cidade: \n");
-scanf("%s",&nome_cidade1);
+scanf("%s",Cidade);
 
 printf("NUmero de habitantes: \n");
-scanf("%i",&numero_de_habitantes1);
+scanf("%i",&populacao1);
 
 printf("Digite a area da cidade: \n");
 scanf("%f",&Area_cidade1);
@@ -52,13 +52,13 @@ printf("Digite uma letra de A a H para o estado: \n");
 scanf(" %c",&letra_cidade2);
 
 printf("Digite a letra ja digitada mais um numero entre 01 e 04 juntos: \n");
-scanf("%s",&letra_numero2);
+scanf("%s",Codigo2);
 
 printf("Digite o nome da cidade: \n");
-scanf("%s",&nome_cidade2);
+scanf("%s",Cidade2);
 
 printf("NUmero de habitantes: \n");
-scanf("%i",&numero_de_habitantes2);
+scanf("%i",&poúlacao2);
 
 printf("Digite a area da cidade: \n");
 scanf("%f",&Area_cidade2);
@@ -69,14 +69,28 @@ scanf("%f",&Pib2);
 printf("Digite quantos pontos turisticos: \n");
 scanf("%i",&Pontos_turisticos2);
 
-float  Densidade_Populacional1 = numero_de_habitantes1/Area_cidade1;
-float  Densidade_Populacional2 = numero_de_habitantes2/Area_cidade2;
-float Pib_per_capita1 = Pib1/numero_de_habitantes1; 
-float Pib_per_capita2 = Pib2/numero_de_habitantes2;
+float  Densidade_Populacional1 = populacao1/Area_cidade1;
+float  Densidade_Populacional2 = poúlacao2/Area_cidade2;
 
-printf("Carta 1:\nEstado:%c\nCódigo:%s\nNome da Cidade:%s\nPopulação:%i\nÁrea:%f\nPIB:%f\nNúmero de Pontos Turísticos:%i\nDensidade Populacional:%f\nPIB per Capita:%f\n",letra_cidade1,letra_numero1,nome_cidade1,numero_de_habitantes1,Area_cidade1,Pib1,Pontos_turisticos1,Densidade_Populacional1,Pib_per_capita1);
+float Pib_per_capita1 = Pib1/populacao1; 
+float Pib_per_capita2 = Pib2/poúlacao2;
 
-printf("Carta 2:\nEstado:%c\nCódigo:%s\nNome da Cidade:%s\nPopulação:%i\nÁrea:%f\nPIB:%f\nNúmero de Pontos Turísticos:%i\nDensidade Populacional:%f\nPIB per Capita:%f\n",letra_cidade2,letra_numero2,nome_cidade2,numero_de_habitantes2,Area_cidade2,Pib2,Pontos_turisticos2,Densidade_Populacional2,Pib_per_capita2);
+unsigned Super_Poder1 =(1/Densidade_Populacional1)+Pib_per_capita1+Pib1+(float)Pontos_turisticos1+Area_cidade1+populacao1;
+unsigned Super_Poder2 =(1/Densidade_Populacional2)+Pib_per_capita2+Pib2+(float)Pontos_turisticos1+Area_cidade2+poúlacao2;
+
+int PopulaçãoComparada= populacao1>poúlacao2;
+int AreaComparada=Area_cidade1>Area_cidade2;
+int PibComparado=Pib1>Pib2;
+int PontosTuristicos=Pontos_turisticos1>Pontos_turisticos2;
+int PibpercaptaComparado=Pib_per_capita1>Pib_per_capita2;
+int DensidadePopulacional=Densidade_Populacional1<Densidade_Populacional2;
+int Super_PoderComparar = Super_Poder1>Super_Poder2;
+
+printf("Carta 1:\nEstado:%c\nCódigo:%s\nNome da Cidade:%s\nPopulação:%i\nÁrea:%.2f\nPIB:%.2f\nNúmero de Pontos Turísticos:%i\nDensidade Populacional:%.2f\nPIB per Capita:%.2f\n",letra_cidade1,Codigo1,Cidade,populacao1,Area_cidade1,Pib1,Pontos_turisticos1,Densidade_Populacional1,Pib_per_capita1);
+
+printf("Carta 2:\nEstado:%c\nCódigo:%s\nNome da Cidade:%s\nPopulação:%i\nÁrea:%.2f\nPIB:%.2f\nNúmero de Pontos Turísticos:%i\nDensidade Populacional:%.2f\nPIB per Capita:%.2f\n",letra_cidade2,Codigo2,Cidade2,poúlacao2,Area_cidade2,Pib2,Pontos_turisticos2,Densidade_Populacional2,Pib_per_capita2);
+
+printf("População:%d\nÁrea:%d\nPIB:%d\nPontos Turísticos:%d\nDensidade Populacional:%d\nPIB per Capita:%d\nSuper Poder:%d\n",PopulaçãoComparada,AreaComparada,PibComparado,PontosTuristicos,DensidadePopulacional,PibpercaptaComparado,Super_PoderComparar);
 
 return 0;
 }
